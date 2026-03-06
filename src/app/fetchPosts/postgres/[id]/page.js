406065877464a2a9b83ac0069490a91194b309e1
nicolodo <nicolasbernard19@gmail.com>
2026-03-06 10:44:33 +0000

    db posts can be viewed

--- src/app/fetchPosts/postgres/[id]/page.js
+++ src/app/fetchPosts/postgres/[id]/page.js
@@ -1,27 +1,46 @@
 
 import pg from "pg"
+import Link from "next/link"
 
 export async function generateMetadata(
-    {params, searchParams}, parent){
+  {params, searchParams}, parent){
     
-    const id = await params
-
-}
-
-export default async function PostgresPostsPage(
+    const slug= await params
+    return{
+      title: 'postid: '+ slug.id,
+      description:"slug",
+    }
+    
+  }
+  
+  export default async function PostgresPostsPage(
     {params}
-) {
-    const id = await params
+  ) {
+    const slug = await params
     const db = new pg.Pool({
-        connectionString: process.env.NEXT_POSTGRES,
+      connectionString: process.env.NEXT_POSTGRES,
     });
-
-
+    const res = (await db.query(
+      'SELECT * FROM POSTS WHERE ID = $1',[slug.id])
+    ).rows;
+    const post = res[0]
+    console.log(post)
+    
+    
     return (
-        <div>
-            <h1>${id}</h1>
-        </div>
-
+      <div>
+      <h1>{`post id: ${slug.id}`}</h1>
+      <h1>{`post title: ${post.title}`}</h1>
+      <h1>{`post content: ${post.content}`}</h1>
+      {post.replyTo && <h1>
+        <Link href={`/fetchPosts/postgres/${post.replyTo}`}>
+          {`post reply to: ${post.replyTo}`}
+        </Link>
+      </h1>  
+      }
+      </div>
+      
     )
-}
-
+  }
+  
+  
